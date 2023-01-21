bd0b2fa5171eee030d38d8193d37433fb5a9e51e
owenlevin <owenlevin@gmail.com>
2023-01-21 15:54:49 -0800

    OrderPickup icons reflect pickups in level

--- Atoms/OrderDisplay/OrderDisplay.gd
+++ Atoms/OrderDisplay/OrderDisplay.gd
@@ -1,7 +1,17 @@
 extends Control
 
 onready var items = get_node("VBoxContainer/HBoxContainer").get_children()
+export(NodePath) var pickups
+onready var pickup_children = get_node(pickups).get_children()
 
+func _ready():
+	for item in items:
+		item.visible = false
+	for pickup in pickup_children:
+		for item in items:
+			if item.name == pickup.pickup_type:
+				item.visible = true 
+	
 func _on_pickup(type):
 	for item in items:
 		if item.name == type:
